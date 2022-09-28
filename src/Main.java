9cabcb78c8c3b954af6837f163ecdf6f7207196d
Amida Fombutu <amida.fombutu@gmail.com>
2022-09-28 10:37:53 -0500

    more

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        int myFavouriteNumber = 2;
+        System.out.println(myFavouriteNumber);
+
+        String myString = "mido";
+        System.out.println(myString);
+
+//       myString = 'm';     ( type: char)
+//       myString = 3.14159;  ( type: float)
+
+        long myNumber;
+//        System.out.println(myNumber);  (not initialized)
+
+
+//        myNumber = 3.14;
+//        System.out.println(myNumber);    (type: float)
+
+        myNumber = 123L;
+        System.out.println(myNumber);   // print out just 123
+
+        myNumber = 123;
+       System.out.println(myNumber);   // out 123
+
+//float myNumber = 3.14;            (myNumber already declared)
+
+float newNumber =  myNumber;
+//newNumber = 3.14;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);  //incremented when print the second time
+//
+
+        int x = 5;
+        System.out.println(++x);  // incremented imidiately
+        System.out.println(x);
+
+//        long class;       ( error identifier expected)
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;                       (string cannot be cast to integer)
+
+//        int three = (int) "three";                  (string cannot be cast to integer)
+
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 4;
+//        x = x+= 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//          y = y *= x
+
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//
+//            x = x /= y
+//            y = y -= x
+
+
+
+
+    }
+
+}
